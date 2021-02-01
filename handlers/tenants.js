14c6f997cf6d02776bd16355e07250ae017a9766
Antoine <antoine.dannemark@protonmail.com>
2021-02-01 14:03:01 +0100

    debug logs

--- handlers/tenants.js
+++ handlers/tenants.js
@@ -19,7 +19,10 @@ const createTenant = async(tenant) => {
 };
 
 const updateTenant = async(id, data) => {
-    console.log(data)
+    console.log('---------')
+    console.log('TENANT HANDLER')
+    console.log(id, data)
+    console.log('---------')
     return await getConnection()
         .createQueryBuilder()
         .update(Tenant)
