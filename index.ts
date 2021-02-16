af660ef66189cbc5fe0a363963c916898153729e
AntoineDannemark <antoine.dannemark@protonmail.com>
2021-02-16 11:30:10 +0100

    remove generateApi logic

--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
-import { createConnection, getRepository, getCustomRepository, Entity } from 'typeorm';
+import { 
+    createConnection, 
+    getCustomRepository, 
+} from 'typeorm';
+
+import { AddressRepository } from './repository/AddressRepository';
+import { PhoneRepository } from './repository/PhoneRepository';
 import { PersonRepository } from "./repository/PersonRepository";
-// import { Tenant } from './entity/Tenant';
+
 import tenantHandlers from './handlers/tenants';
 import peopleHandlers from './handlers/people';
+
 import { Person } from './entity/Person';
 import { Address } from './entity/Address';
 import { Phone } from './entity/Phone';
@@ -21,9 +28,11 @@ const initDB = async(platform: platform) => {
         const personRepository = getCustomRepository(PersonRepository); 
         const peopleCount = await personRepository.count()
 
+        console.warn('People count: ', peopleCount)
+        
         if (!peopleCount) await createJohnDoe()
         
-        const john = await personRepository.findByName('john', 'doe'); // same as const user = new User();
+        const john = await personRepository.findByName('john', 'doe');
 
         console.log('---------------------------')
         console.log(john)
@@ -41,37 +50,8 @@ const initDB = async(platform: platform) => {
     })
 }
 
-const getMethods = (obj: object) => Object.getOwnPropertyNames(obj).filter(item => item !== 'constructor' && typeof obj[item] === 'function') 
-
-const generateApi = async function(repository: any =  PersonRepository) {
-    let repo = Object.getPrototypeOf(getCustomRepository(repository))
-    const methods = await getMethods(repo);
-   
-    return methods;
-}
-
 const api = {
     initDB,
-    generateApi,
-    /*
-
-    TODO - if possible, 
-
-    => generate the functions programmatically by looping over the repo's methods
-    after all we're only passing through the typed arguments, and awaiting the homonym's call on the appropriate repo
-    
-    Imagine something like:
-    
-    const getEntityCustomRepo = Entity => CustomRepo;
-  
-    const generateApi = CustomRepo => CustomApi;
-
-    api: {
-        person: generateApi(getEntityCustomRepo(Person)),
-        (...)
-    }
-    
-    */
     person: {
         findByName: async(firstname: string, lastname: string) => {
             const personRepository = getCustomRepository(PersonRepository); 
@@ -110,18 +90,17 @@ export type Api = typeof api;
 export default api;
 
 const createJohnDoe = async () => {
-    console.log('/*/*/*/* CREATE JOHN DOE')
     const personRepository = getCustomRepository(PersonRepository); 
-    const phoneRepository = getRepository(Phone);
-    const addressRepository = getRepository(Address);
-    
+    const phoneRepository = getCustomRepository(PhoneRepository);
+    const addressRepository = getCustomRepository(AddressRepository);
+
     const phone = phoneRepository.create({
         prefix: 32,
         number: 670400
     })
 
     await phoneRepository.save(phone)
-    console.log('/*/*/*/* SAVE PHONE')
+
     const address = addressRepository.create({
         street: "Rue Antoine",
         number: 76,
@@ -131,7 +110,7 @@ const createJohnDoe = async () => {
     })
     
     await addressRepository.save(address)
-    console.log('/*/*/*/* SAVE ADDRESS')
+
     const person = personRepository.create({
         firstname: "john",
         lastname: "doe",
@@ -143,9 +122,8 @@ const createJohnDoe = async () => {
         bankAccount: 'BE674567321564',
         bankCode: 'BBRUEB',
         phones: [phone],
-        addresses: [address]
+        addresses: [address],
     })
 
     await personRepository.save(person)
-    console.log('/*/*/*/* SAVE JOHN')
 }
