d75714f394f2e288f6e54da35d56930fd7d57587
Antoine <antoine.dannemark@protonmail.com>
2021-02-22 11:16:59 +0100

    person.fetchAll : fix leftjoin phones

--- person/fetchAll.ts
+++ person/fetchAll.ts
@@ -1,14 +1,13 @@
-import { getConnection } from 'typeorm';
+import { getRepository } from 'typeorm';
 import { Person } from './index';
 
 // const lightQueryCols = ["id, firstname, lastname, email"];
-const fullQueryCols = ["id, firstname, lastname, birthDate, birthPlace, email, gender, bankAccount, bankCode, phone.number, phone.prefix"];
+// const fullQueryCols = ["p.id, p.firstname, p.lastname, p.birthDate, p.birthPlace, p.email, p.gender, p.bankAccount, p.bankCode, phone.number, phone.prefix"];
 
 export default async() => {
-    await getConnection()
-        .createQueryBuilder()
-        .select(fullQueryCols)
-        .from(Person, "person")
-        .leftJoinAndSelect("person.phones","phone")
-        .execute()
+    return await getRepository(Person)
+        .createQueryBuilder("pe")
+        .leftJoinAndSelect("pe.phones","ph")
+        // .select(["pe.id", "pe.firstname", "pe.lastname", "ph.prefix", "ph.number"])
+        .getMany()
 }
