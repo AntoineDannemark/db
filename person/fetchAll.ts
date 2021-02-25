21b5d0bb13776fdc4faca04021659f5cd21ab1bd
greg <gregory@referencefitness.com>
2021-02-25 12:35:03 +0100

    person fetchAll: use new connection method

--- person/fetchAll.ts
+++ person/fetchAll.ts
@@ -1,13 +1,18 @@
+import Database from './../Database';
 import { getRepository } from 'typeorm';
 import { Person } from './index';
 
 // const lightQueryCols = ["id, firstname, lastname, email"];
 // const fullQueryCols = ["p.id, p.firstname, p.lastname, p.birthDate, p.birthPlace, p.email, p.gender, p.bankAccount, p.bankCode, phone.number, phone.prefix"];
 
-export default async() => {
-    return await getRepository(Person)
-        .createQueryBuilder("pe")
-        .leftJoinAndSelect("pe.phones","ph")
+export default async () => {
+    let connection = await Database.getConnectionInstance()
+
+    return await connection
+        .createQueryBuilder()
+        .select("person")
+        .from(Person, "person")
+        .leftJoinAndSelect("person.phones", "ph")
         // .select(["pe.id", "pe.firstname", "pe.lastname", "ph.prefix", "ph.number"])
         .getMany()
 }
