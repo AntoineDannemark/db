ba4e3e320916a6e8674f366ecf03beb9c9ac641e
Gregory Ludes <gregory.ludes@gmail.com>
2021-03-02 15:28:27 +0100

    person addPhone: adapt to new connection

--- person/actions/addPhone.ts
+++ person/actions/addPhone.ts
@@ -1,18 +1,27 @@
-import { getConnection } from 'typeorm';
+/**
+ * @name person/phone
+ * @method POST
+*/
+
 import { Person } from '../Person';
-import phoneApi, { IPhone } from '../../phone/index';
+import phoneApi from '../../phone/index';
+import Database from '../../Database';
 
 /**
  * Adds a phone number to a person
  * @param {number} id - the id of the person
  * @param {IPhone} phone - the phone number to create
  */
-export default async (id: number, phone: IPhone) => {    
+export default async ({ id, phone }) => {
+    let connection = await Database.getConnectionInstance()
+
     const phoneId = await phoneApi.create(phone)
-    return await getConnection()
+    await connection
         .createQueryBuilder()
-        .relation(Person,  "phones")
+        .relation(Person, "phones")
         .of(id)
-        .add(phoneId)    
+        .add(phoneId)
+
+    return { phoneId }
 }
 
