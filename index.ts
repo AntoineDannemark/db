d9ee995e3f2d68c1dc040a52e28db50aef667455
AntoineDannemark <antoine.dannemark@protonmail.com>
2021-03-03 12:14:23 +0100

    Revert "test cross-fetch"
    This reverts commit d613cd36586acfbfdae6eb287a26fbfc9f1c496c.

--- index.ts
+++ index.ts
@@ -1,15 +1,12 @@
 import personRoutes from './person';
 import phoneRoutes from './phone';
-import addressRoutes from './address';
 import Database from './Database';
-import fetch from 'cross-fetch';
 
-const api = {
+// Ici si sls 
+
+const api = {    
     utils: {
-        env: async() => {
-            const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-            return res;
-        },
+        env: () => 'prout',
         testDBConnection: async() => {
             try {
                 let connection = await Database.getConnectionInstance();
@@ -26,8 +23,7 @@ const api = {
         },
     },
     person: { ...personRoutes },
-    phone: { ...phoneRoutes },
-    address: { ...addressRoutes },
+    phone: { ...phoneRoutes }
 }
 
 export type Api = typeof api;
