63df2f56d0c187b6b0f05fd55e862a56a2b75e43
Antoine <antoine.dannemark@protonmail.com>
2021-03-05 21:49:23 +0100

    getEndpoints: fix param type + remove useless try catch

--- utils/actions/getEndpoint.ts
+++ utils/actions/getEndpoint.ts
@@ -1,12 +1,10 @@
-// import log from 'electron-log';
-export default async (isElectron): Promise<void> => {
+export default async (isElectron: boolean): Promise<void> => {
     if (isElectron) {
-        const Store = require('electron-store');
+            const Store = require('electron-store');
+            const store = new Store();
 
-        const store = new Store();
-
-        // TODO Check return value + use for async
-        return await store.get('dbEndpoint');
+            // TODO Check return value + use for async
+            return await store.get('dbEndpoint');
     } else {
         const Storage = require('@capacitor/core').Plugins.Storage;
         
