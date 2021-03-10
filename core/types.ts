f8647f5903667c13b974a1ddc0be3e82c6e79e54
AntoineDannemark <antoine.dannemark@protonmail.com>
2021-03-10 09:46:55 +0100

    declare type Endpoint + DB_HOSTING constant in /core

---
+++ core/types.ts
@@ -0,0 +1,10 @@
+import { DB_HOSTING } from './constants';
+
+const { LOCAL, SLS } = DB_HOSTING;
+
+export type DbHosting = typeof LOCAL | typeof SLS | undefined;
+export type SlsEndpoint = string | undefined;
+export type Endpoint = {
+    dbHosting: DbHosting;
+    slsEndpoint: SlsEndpoint;
+}
