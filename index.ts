166112dd7d519ff2df4c328ed3365aa01dc4e550
AntoineDannemark <antoine.dannemark@protonmail.com>
2021-03-11 08:55:27 +0100

    @index.ts - refactor with getSlsApi(endpoint)

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import utilsRoutes from './utils';
 import storageApi from './../storage';
 import { isPlatform } from '@ionic/react';
 
-const isLocalHosting = storageApi.getEndpoint(isPlatform("electron")).dbHosting === "local"
+const endpoint = storageApi.getEndpoint(isPlatform("electron"))
 
 let api;
 
@@ -14,20 +14,39 @@ const fetchGet = async (actionUrl: any, params: any) => {
 	return fetch(url.toString()).then(r => r.json());
 };
 
-const slsApi = {
-	// -start - don't remove this comment used to generate api codeaddress: {},
-	person: {
-		addAddress: async (...args: any[]) => await fetch('https://9lrwatxyfl.execute-api.eu-west-2.amazonaws.com/dev/api/person/address', {method:'POST', body: JSON.stringify(args)}).then(r => r.json()),
-		addPhone: async (...args: any[]) => await fetch('https://9lrwatxyfl.execute-api.eu-west-2.amazonaws.com/dev/api/person/phone', {method:'POST', body: JSON.stringify(args)}).then(r => r.json()),
-		create: async (...args: any[]) => await fetch('https://9lrwatxyfl.execute-api.eu-west-2.amazonaws.com/dev/api/person', {method:'POST', body: JSON.stringify(args)}).then(r => r.json()),
-		fetchAll: async (...args: any[]) => await fetchGet('https://9lrwatxyfl.execute-api.eu-west-2.amazonaws.com/dev/api/persons', args),
-		softDelete: async (...args: any[]) => await fetch('https://9lrwatxyfl.execute-api.eu-west-2.amazonaws.com/dev/api/person/softDelete', {method:'POST', body: JSON.stringify(args)}).then(r => r.json())
-	},
-	phone: {},
-	utils: {
-		testConnection: async (...args: any[]) => await fetchGet('https://9lrwatxyfl.execute-api.eu-west-2.amazonaws.com/dev/api/utils/test', args)
-	}
-// don't remove this comment used to generate api code -end
+// const slsApi = {
+// 	// -start - don't remove this comment used to generate api codeaddress: {},
+// 	person: {
+// 		addAddress: async (...args: any[]) => await fetch('https://9lrwatxyfl.execute-api.eu-west-2.amazonaws.com/dev/api/person/address', {method:'POST', body: JSON.stringify(args)}).then(r => r.json()),
+// 		addPhone: async (...args: any[]) => await fetch('https://9lrwatxyfl.execute-api.eu-west-2.amazonaws.com/dev/api/person/phone', {method:'POST', body: JSON.stringify(args)}).then(r => r.json()),
+// 		create: async (...args: any[]) => await fetch('https://9lrwatxyfl.execute-api.eu-west-2.amazonaws.com/dev/api/person', {method:'POST', body: JSON.stringify(args)}).then(r => r.json()),
+// 		fetchAll: async (...args: any[]) => await fetchGet('https://9lrwatxyfl.execute-api.eu-west-2.amazonaws.com/dev/api/persons', args),
+// 		softDelete: async (...args: any[]) => await fetch('https://9lrwatxyfl.execute-api.eu-west-2.amazonaws.com/dev/api/person/softDelete', {method:'POST', body: JSON.stringify(args)}).then(r => r.json())
+// 	},
+// 	phone: {},
+// 	utils: {
+// 		testConnection: async (...args: any[]) => await fetchGet('https://9lrwatxyfl.execute-api.eu-west-2.amazonaws.com/dev/api/utils/test', args)
+// 	}
+// // don't remove this comment used to generate api code -end
+// }
+
+const getSlsApi = (endpoint: string) => {
+    return {
+
+            // -start - don't remove this comment used to generate api codeaddress: {},
+            person: {
+                addAddress: async (...args: any[]) => await fetch(`${endpoint}/person/address`, {method:'POST', body: JSON.stringify(args)}).then(r => r.json()),
+                addPhone: async (...args: any[]) => await fetch(`${endpoint}/person/phone`, {method:'POST', body: JSON.stringify(args)}).then(r => r.json()),
+                create: async (...args: any[]) => await fetch(`${endpoint}/person`, {method:'POST', body: JSON.stringify(args)}).then(r => r.json()),
+                fetchAll: async (...args: any[]) => await fetchGet(`${endpoint}/persons`, args),
+                softDelete: async (...args: any[]) => await fetch(`${endpoint}/person/softDelete`, {method:'POST', body: JSON.stringify(args)}).then(r => r.json())
+            },
+            phone: {},
+            utils: {
+                testConnection: async (...args: any[]) => await fetchGet(`${endpoint}/utils/test`, args)
+            }
+        // don't remove this comment used to generate api code -end
+    }
 }
 
 const localApi = {    
@@ -36,7 +55,7 @@ const localApi = {
     phone: { ...phoneRoutes }
 }
 
-api = isLocalHosting ? localApi : slsApi;
+api = endpoint === "local" ? localApi : getSlsApi(endpoint);
 
 export type Api = typeof api;
 export default api;
