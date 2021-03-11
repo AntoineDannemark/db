c2b6d2f3bd38db59cfefc39695d11d5880b45c49
AntoineDannemark <antoine.dannemark@protonmail.com>
2021-03-11 14:07:47 +0100

    remove commented slsApi

--- index.ts
+++ index.ts
@@ -14,25 +14,8 @@ const fetchGet = async (actionUrl: any, params: any) => {
 	return fetch(url.toString()).then(r => r.json());
 };
 
-// const slsApi = {
-// 	// -start - don't remove this comment used to generate api codeaddress: {},
-// 	person: {
-// 		addAddress: async (...args: any[]) => await fetch('https://9lrwatxyfl.execute-api.eu-west-2.amazonaws.com/dev/api/person/address', {method:'POST', body: JSON.stringify(args)}).then(r => r.json()),
-// 		addPhone: async (...args: any[]) => await fetch('https://9lrwatxyfl.execute-api.eu-west-2.amazonaws.com/dev/api/person/phone', {method:'POST', body: JSON.stringify(args)}).then(r => r.json()),
-// 		create: async (...args: any[]) => await fetch('https://9lrwatxyfl.execute-api.eu-west-2.amazonaws.com/dev/api/person', {method:'POST', body: JSON.stringify(args)}).then(r => r.json()),
-// 		fetchAll: async (...args: any[]) => await fetchGet('https://9lrwatxyfl.execute-api.eu-west-2.amazonaws.com/dev/api/persons', args),
-// 		softDelete: async (...args: any[]) => await fetch('https://9lrwatxyfl.execute-api.eu-west-2.amazonaws.com/dev/api/person/softDelete', {method:'POST', body: JSON.stringify(args)}).then(r => r.json())
-// 	},
-// 	phone: {},
-// 	utils: {
-// 		testConnection: async (...args: any[]) => await fetchGet('https://9lrwatxyfl.execute-api.eu-west-2.amazonaws.com/dev/api/utils/test', args)
-// 	}
-// // don't remove this comment used to generate api code -end
-// }
-
 const getSlsApi = (endpoint: string) => {
     return {
-
             // -start - don't remove this comment used to generate api codeaddress: {},
             person: {
                 addAddress: async (...args: any[]) => await fetch(`${endpoint}/person/address`, {method:'POST', body: JSON.stringify(args)}).then(r => r.json()),
