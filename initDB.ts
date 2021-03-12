8601a59a0dcba7660924d1df714e06e1762ba119
Antoine <antoine.dannemark@protonmail.com>
2021-03-12 19:17:44 +0100

    initDB - refactor with utils/platforms

--- initDB.ts
+++ initDB.ts
@@ -1,12 +1,18 @@
 import { createConnection } from 'typeorm';
 
+import { log } from './utils/logger';
+
 import { Person } from './person';
 import { Address } from './address';
 import { Phone } from './phone';
 
-export default async(isLocalEndpoint: boolean, isCordova: boolean) => {   
-    const type = isLocalEndpoint && isCordova ? "cordova" : "better-sqlite3",
-        database = isLocalEndpoint ? "medieval.db": "/mnt/efs/medieval.db";
+import { isCordova, isServerless } from './utils/platforms';
+
+export default async() => {   
+    const type = isCordova() ? "cordova" : "better-sqlite3",
+        database = isServerless() ? "/mnt/efs/medieval.db" : "medieval.db";
+
+    log(`initDB - will create connection with driver ${type}`, "api");
 
     return await createConnection({
         type,
