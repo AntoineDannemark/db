3454de7b5545c36a162292198bd538721b49e4dd
Antoine <antoine.dannemark@protonmail.com>
2021-03-12 13:06:49 +0100

    headers content-type app/json for slsApi

--- index.ts
+++ index.ts
@@ -14,21 +14,25 @@ const fetchGet = async (actionUrl: any, params: any) => {
 	return fetch(url.toString()).then(r => r.json());
 };
 
+const headers = new Headers({
+    "Content-Type": "application/json",
+});
+
 const getSlsApi = (endpoint: string) => {
     return {
             // -start - don't remove this comment used to generate api codeaddress: {},
-            person: {
-                addAddress: async (...args: any[]) => await fetch(`${endpoint}/person/address`, {method:'POST', body: JSON.stringify(args)}).then(r => r.json()),
-                addPhone: async (...args: any[]) => await fetch(`${endpoint}/person/phone`, {method:'POST', body: JSON.stringify(args)}).then(r => r.json()),
-                create: async (...args: any[]) => await fetch(`${endpoint}/person`, {method:'POST', body: JSON.stringify(args)}).then(r => r.json()),
-                fetchAll: async (...args: any[]) => await fetchGet(`${endpoint}/persons`, args),
-                softDelete: async (...args: any[]) => await fetch(`${endpoint}/person/softDelete`, {method:'POST', body: JSON.stringify(args)}).then(r => r.json())
-            },
-            phone: {},
-            utils: {
-                testConnection: async (...args: any[]) => await fetchGet(`${endpoint}/utils/test`, args)
-            }
-        // don't remove this comment used to generate api code -end
+	person: {
+		addAddress: async (...args: any[]) => await fetch(`${endpoint}/api/person/address`, {method:'POST', headers, body: JSON.stringify(args[0])}).then(r => r.json()),
+		addPhone: async (...args: any[]) => await fetch(`${endpoint}/api/person/phone`, {method:'POST', headers, body: JSON.stringify(args[0])}).then(r => r.json()),
+		create: async (...args: any[]) => await fetch(`${endpoint}/api/person`, {method:'POST', headers, body: JSON.stringify(args[0])}).then(r => r.json()),
+		fetchAll: async (...args: any[]) => await fetchGet(`${endpoint}/api/persons`, args),
+		softDelete: async (...args: any[]) => await fetch(`${endpoint}/api/person/softDelete`, {method:'POST', headers, body: JSON.stringify(args[0])}).then(r => r.json())
+	},
+	phone: {},
+	utils: {
+		testConnection: async (...args: any[]) => await fetchGet(`${endpoint}/api/utils/test`, args)
+	}
+// don't remove this comment used to generate api code -end
     }
 }
 
