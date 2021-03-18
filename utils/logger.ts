06257881aa4b33af988c6cd25fa4a02bdd0283b3
AntoineDannemark <antoine.dannemark@protonmail.com>
2021-03-18 12:50:28 +0100

    @logger - Fix electorn-log import error if not on electron

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,19 +1,17 @@
 import { isServerless, isElectronMain } from './platforms';
-// Prefix logs with [api]
-// Cordova logs to the console, 
-// electron logs 
-// 
 
 export const log = (value: string, prefix: string) => {
-    // Includes electron and mobile apps that 
     if (isServerless()) {
         // TODO Check greg
         return console.log(`[${prefix}] - ${value}`);
     } else if (isElectronMain()) {
-        const log = require('electron-log');
+        try {
+            const log = require('electron-log');
 
-        return log.info(`[${prefix}] - ${value}`);
-       // Renderer logs will appear in the console per default, use console.log
+            return log.info(`[${prefix}] - ${value}`);
+        } catch (err) {
+            return console.log(`[${prefix}] - ${value}`);
+        }
     } 
 
     return console.log(`[${prefix}] - ${value}`);
